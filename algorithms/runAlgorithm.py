81b26c44e208fc2f6a35860261bf0550d289ec4f
Bubbleblob <Kylemach168@gmail.com>
2023-10-05 12:12:39 -0700

    added choose algorithm panel

--- algorithms/runAlgorithm.py
+++ algorithms/runAlgorithm.py
@@ -20,12 +20,17 @@ def runAlgorithm(window, width):
     run_algorithm_button = Button(400, 850, 100, 50, "run")
     choose_algorithm_button = Button(550, 850, 110, 50, "algorithm")
 
-    # a_star_button = Button(100, 850, 100, 50, "A*")
-    # dijkstra_button = Button(100, 850, 100, 50, "Dijkstra")
-    # bfs_button = Button(100, 850, 100, 50, "BFS")
+    a_star_button = Button(100, 850, 100, 50, "A*")
+    dijkstra_button = Button(250, 850, 100, 50, "Dijkstra")
+    bfs_button = Button(400, 850, 100, 50, "BFS")
+
+    main_panel_flag = True  # used to display and run functionality of main panel buttons
+    algorithm_panel_flag = False  # used to display and run functionality of choosing algorithm buttons
 
     while run:
         draw(window, grid, ROWS, width, update_grid)
+        pygame.draw.rect(window, LIGHT_GREY, update_control_panel)
+        pygame.draw.line(window, GREY, (0, width), (width, width))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -80,20 +85,41 @@ def runAlgorithm(window, width):
                     start = None
                     end = None
 
-        if main_menu_button.draw(window):
-            run = False
-        if reset_button.draw(window):
-            resetGrid(grid)
-            start = None
-            end = None
-        if run_algorithm_button.draw(window):
-            for row in grid:
-                for node in row:
-                    node.update_neighbors(grid) # update neighbors based on change in barriers
-            
-            aStar(lambda: draw(window, grid, ROWS, width, update_grid), grid, start, end)
-        if choose_algorithm_button.draw(window):
-            print("4")                                                                                          # FINISH THIS PART
+        # buttons for main panel
+        if main_panel_flag:
+            if main_menu_button.draw(window):
+                run = False
+                
+            if reset_button.draw(window):
+                resetGrid(grid)
+                start = None
+                end = None
+
+            if run_algorithm_button.draw(window):
+                if start and end:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors(grid) # update neighbors based on change in barriers
+                    
+                    aStar(lambda: draw(window, grid, ROWS, width, update_grid), grid, start, end)
+
+            if choose_algorithm_button.draw(window):
+                main_panel_flag = False
+                algorithm_panel_flag = True
+
+        # buttons for choosing algorithm panel
+        if algorithm_panel_flag: ################################################################################# FINISH THIS
+            if a_star_button.draw(window):
+                main_panel_flag = True
+                algorithm_panel_flag = False
+
+            if dijkstra_button.draw(window):
+                main_panel_flag = True
+                algorithm_panel_flag = False
+
+            if bfs_button.draw(window):
+                main_panel_flag = True
+                algorithm_panel_flag = False
         
         pygame.display.update(update_control_panel)
     
